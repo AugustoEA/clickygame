464be21079513a7492881af26815b2e0fa7f4fbc
Augusto <gutoea@gmail.com>
2019-11-01 17:25:14 -0500

    cast.js

---
+++ src/cast.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-expressions */
+[
+  {
+    "id": "1",
+    "name": "Yautja (Predator)",
+    "src": "https://cdn.vox-cdn.com/thumbor/i4yuOFwegxlzaTzOwPT6lcRlSFQ=/124x0:1474x900/1200x800/filters:focal(124x0:1474x900)/cdn.vox-cdn.com/uploads/chorus_image/image/48838179/predator.0.0.jpg"
+  },
+  {
+    "id": "2",
+    "name": "Major Alan 'Dutch' Schaefer",
+    "src": "https://townsquare.media/site/442/files/2013/08/Predator.jpg?w=980&q=75"
+  },
+  {
+    "id": "3",
+    "name": "George Dillon",
+    "src": "https://imgc.allpostersimages.com/img/print/u-g-PJ5FAH0.jpg?w=550&h=550&p=0"
+  },
+  {
+    "id": "4",
+    "name": "Anna",
+    "src": "https://www.avpgalaxy.net/wordpress/wp-content/uploads/2019/06/what-happened-to-anna-01-701x464.jpg"
+  },
+  {
+    "id": "5",
+    "name": "Jorge 'Pancho' Ramirez",
+    "src": "https://66.media.tumblr.com/fb29ff47f3caf626cd01c0d6af28e5eb/tumblr_pg8r79LVQb1svlz2ko1_1280.jpg"
+  },
+  {
+    "id": "6",
+    "name": "Mac Eliot",
+    "src": "https://i.pinimg.com/originals/48/73/38/487338b9451d2970e9425f0227447c8c.jpg"
+  },
+  {
+    "id": "7",
+    "name": "Blain Cooper",
+    "src": "https://vignette.wikia.nocookie.net/avp/images/7/7a/Blain.png/revision/latest?cb=20130413101849"
+  },
+  {
+    "id": "8",
+    "name": "Billy Sole",
+    "src": "http://www.coastaldigest.com/sites/default/files/Sonny.jpg"
+  },
+  {
+    "id": "9",
+    "name": "Rick Hawkins",
+    "src": "https://img.cinemablend.com/filter:scale/quill/5/9/3/5/c/7/5935c7087993aea0f56413c8efc1ad76d8a77ba0.jpg?mw=600"
+  }
+]
