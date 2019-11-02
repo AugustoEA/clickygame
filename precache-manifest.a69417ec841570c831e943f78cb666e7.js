d8b18266b0e07549d46e366d103c54dd8353f09f
Augusto <gutoea@gmail.com>
2019-11-01 19:49:29 -0500

    Updates

---
+++ precache-manifest.a69417ec841570c831e943f78cb666e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf8da10e0f11e7bf5db8f8182d3b6cb",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "148c1d0ab991d5a14974",
+    "url": "/clickygame/static/css/main.16b1f4c2.chunk.css"
+  },
+  {
+    "revision": "274ed3eb64297e51795e",
+    "url": "/clickygame/static/js/2.2b3e21ef.chunk.js"
+  },
+  {
+    "revision": "148c1d0ab991d5a14974",
+    "url": "/clickygame/static/js/main.08a7c50d.chunk.js"
+  },
+  {
+    "revision": "b4ed2c23f69c9b03baae",
+    "url": "/clickygame/static/js/runtime-main.f70fee65.js"
+  }
+]);
